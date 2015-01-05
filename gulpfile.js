f11afc522b145868d4173d5d64e3887af4b91136
André Felipe Dias <andref.dias@pronus.eng.br>
2015-01-05 19:10:35 -0200

    melhoria na geração de estilos .css através do comando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
+var stylus_normalize = require('stylus-normalize');
+var elf_grid = require('elf-grid');
+var rupture = require('rupture');
+var watch = require('gulp-watch');
 var comprimir = true;
 var paths = {
     styles: [
         'server/static/styles/main.styl',
-        'server/static/styles/menus.styl'
+        'server/static/styles/menus.styl',
+        'server/static/styles/headers_footers.styl'
     ],
     dest: 'server/static/css'
 };
 
 gulp.task('stylus', function() {
     return gulp.src(paths.styles)
-        .pipe(stylus({compress: comprimir}))
+        .pipe(stylus(
+            {
+                use: [stylus_normalize(), elf_grid(), rupture()],
+                compress: comprimir
+            })
+        )
         .pipe(autoprefixer())
         .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('watch', function() {
+    return watch(paths.styles, function() {
+        return gulp.start('stylus');
+    });
 });
 
 gulp.task('default', ['stylus']);
