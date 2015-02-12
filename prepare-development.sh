8549d4880db9bd84686756d90089c231724c1f9c
André Felipe Dias <andref.dias@pronus.eng.br>
2015-02-11 23:29:42 -0200

    atualização na configuração de uma máquina de desenvolvimento

--- prepare-development.sh
+++ prepare-development.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# apt-get install -y nodejs npm
+# npm install -g gulp
 source `which virtualenvwrapper.sh`
-mkvirtualenv codebox -p `which python3`
-pip install -r requirements.txt
+mkvirtualenv codelab -p `which python3`
+pip3 install -r requirements.txt
 npm install
-gulp
 
+# http://askubuntu.com/questions/488529/pyvenv-3-4-error-returned-non-zero-exit-status-1
+# pyvenv-3 --without-pip venv
+# source venv/bin/activate
+# curl https://bootstrap.pypa.io/get-pip.py | python
+# deactivate
+# source venv/bin/activate
+# pip install -r requirements.txt
