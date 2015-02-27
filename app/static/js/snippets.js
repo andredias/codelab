c085658a252d144eb20bd44ae8e5e26e9e08d6a1
André Felipe Dias <andref.dias@pronus.eng.br>
2015-02-27 14:01:01 -0300

    expande/contrai detalhes dos trechos de projetos

--- app/static/js/snippets.js
+++ app/static/js/snippets.js
@@ -4,23 +4,23 @@ function setActive(elem) {
 
 function showSnippets(ref, option) {
     setActive(ref);
-    $('article').css('display', 'none');
-    $('article#' + option).css('display', '');
+    $('article').addClass('hidden');
+    $('article#' + option).removeClass('hidden');
     return false;
 }
 
 
 function enableAll(ref) {
     setActive(ref);
-    $('section.project').css('display', '');
+    $('section.project').removeClass('hidden');
 }
 
 
 function filterLanguage(ref) {
     setActive(ref);
     var language = $(ref).text().toLowerCase();
-    $('section.project[data-language="' + language + '"]').css('display', '');
-    $('section.project[data-language!="' + language + '"]').css('display', 'none');
+    $('section.project[data-language="' + language + '"]').removeClass('hidden');
+    $('section.project[data-language!="' + language + '"]').addClass('hidden');
     return false;
 }
 
@@ -29,5 +29,29 @@ function getVisitedProjects(ref, option) {
     $.get('/visited', {'option': option}, function(data) {
         $('#' + option).html(data);
         showSnippets(ref, option);
+        bindDetailButtons();
     });
 }
+
+
+function showDetail() {
+    var parent = $(this).parents('section.project')[0];
+    $(parent).removeClass('detail-hidden');
+}
+
+
+function hideDetail() {
+    var parent = $(this).parents('section.project')[0];
+    $(parent).addClass('detail-hidden');
+}
+
+
+function bindDetailButtons() {
+    $('i.icon-zoom-in').on('click', showDetail);
+    $('i.icon-zoom-out').on('click', hideDetail);
+}
+
+
+$(function() {
+    bindDetailButtons();
+});
