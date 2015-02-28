8137deeb254fc8ce9b923d70d4a6aa98a7d5912c
André Felipe Dias <andref.dias@pronus.eng.br>
2015-02-28 18:43:58 -0300

    paginação dos exemplos de projetos

--- app/static/js/snippets.js
+++ app/static/js/snippets.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function setActive(elem) {
     $(elem).addClass('active').siblings().removeClass('active');
 }
@@ -6,6 +8,7 @@ function showSnippets(ref, option) {
     setActive(ref);
     $('article').addClass('hidden');
     $('article#' + option).removeClass('hidden');
+    setMaxPage();
     return false;
 }
 
@@ -13,6 +16,7 @@ function showSnippets(ref, option) {
 function enableAll(ref) {
     setActive(ref);
     $('section.project').removeClass('hidden');
+    setMaxPage();
 }
 
 
@@ -21,6 +25,7 @@ function filterLanguage(ref) {
     var language = $(ref).text().toLowerCase();
     $('section.project[data-language="' + language + '"]').removeClass('hidden');
     $('section.project[data-language!="' + language + '"]').addClass('hidden');
+    setMaxPage();
     return false;
 }
 
@@ -52,7 +57,38 @@ function bindDetailButtons() {
     $('i.icon-zoom-out').on('click', hideDetail);
 }
 
+var projList;
+
+function calcPage(index) {
+    var items_per_page = 5;
+    if (index == 0) {
+        return 1;
+    }
+    return Math.ceil(index / items_per_page);
+};
+
+function setMaxPage() {
+    projList = $('article:not(.hidden) section.project:not(.hidden)');
+    var max_page = calcPage(projList.length);
+    $('.pagination').jqPagination('option', {'max_page': max_page, 'current_page': 1});
+};
+
+
+function paginate(page) {
+    $.each(projList, function(index, elem) {
+        if (calcPage(index + 1) != page) {
+            $(elem).addClass('paginate-hidden');
+        } else {
+            $(elem).removeClass('paginate-hidden');
+        }
+    });
+};
+
 
 $(function() {
     bindDetailButtons();
+    $('.pagination').jqPagination({
+        paged : paginate
+    });
+    setMaxPage();
 });
