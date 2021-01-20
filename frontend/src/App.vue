42435247e309b87b8efbce4916971a7c914fcdb9
André Felipe Dias <andre.dias@pronus.io>
2021-01-19 22:36:10 -0300

    home e footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
 Header
 router-view
+Footer
 </template>
 
 <script>
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 export default {
     name: "Code Lab",
     components: {
         Header,
+        Footer
     }
 }
 </script>
