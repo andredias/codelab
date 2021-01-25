e7d4f984663575b5225340bb25e2e059ea51813d
André Felipe Dias <andre.dias@pronus.io>
2021-01-25 14:17:56 -0300

    corrige formataçao do svg

--- frontend/src/views/Dojo.vue
+++ frontend/src/views/Dojo.vue
@@ -11,7 +11,7 @@
         .projeto
             .titulo
                 h1 Título do Projeto
-                svg(class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor")
+                svg.icon.link(fill="none" viewBox="0 0 24 24" stroke="currentColor")
                     path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z")
             .descricao.box
                 p
