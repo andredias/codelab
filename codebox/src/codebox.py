52a6717cc4275cb22d28bd12a6eeadf7377de35f
André Felipe Dias <andre.dias@pronus.io>
2021-02-03 01:58:26 -0300

    project codebox container imported from external repo

---
+++ codebox/src/codebox.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import json
+import shlex
+from subprocess import call, check_output, Popen, PIPE, TimeoutExpired
+from tempfile import TemporaryDirectory
+from metrics.lint import lint
+from metrics.metrics import collect_metrics
+
+
+def run(sourcetree=None, commands=None, input_=None):
+    '''
+    commands = [(phase, line, timeout), ...]
+    '''
+    input_ = input_ or ''
+    sourcetree = sourcetree or {}
+    commands = commands or []
+    response = {}
+    with TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        if sourcetree:
+            save_sourcetree(sourcetree)
+            response.update(evaluate(sourcetree.keys()))
+        # não executar nenhum comando como root
+        ruid, euid, suid = os.getresuid()
+        is_root = ruid == 0
+        if is_root:
+            os.system('chown -R 1000:1000 %s' % tempdir)
+            os.setresuid(1000, 1000, 0)  # run as 'user'
+        try:
+            response.update(exec_commands(commands, input_))
+        finally:
+            if is_root:
+                os.setresuid(ruid, euid, suid)  # back to 'root'
+    return response
+
+
+def save_sourcetree(sourcetree, destdir=''):
+    '''
+    Save sources to a temporary directory
+    '''
+    for name, source in sourcetree.items():
+        filename = os.path.join(destdir, name.lstrip(os.path.sep))
+        directory = os.path.dirname(filename)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+        with open(filename, mode='w', encoding='utf-8') as f:
+            f.write(source)
+
+
+def evaluate(filenames, ref_dir=''):
+    result = {}
+    result['lint'] = {}
+    result['metrics'] = {}
+    for filename in filenames:
+        tempfilename = os.path.join(ref_dir, filename)
+        lint_result = lint(tempfilename)
+        if lint_result:
+            result['lint'][filename] = lint_result
+        metrics_result = collect_metrics(tempfilename)
+        if metrics_result:
+            result['metrics'][filename] = metrics_result
+    return result
+
+
+def exec_commands(commands, input_=None, ref_dir=''):
+    if ref_dir:
+        os.chdir(ref_dir)
+    response = {}
+    exit_code = 0
+    for phase, command, timeout in commands:
+        # TODO: substituir por run quando chegar o Python 3.5
+        # trecho abaixo baseado no exemplo de:
+        # https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
+        if isinstance(command, str):
+            command = shlex.split(command)
+        try:
+            proc = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+            output, errors = proc.communicate(input=input_, timeout=timeout)
+            exit_code = proc.returncode
+        except FileNotFoundError:
+            output = ''
+            errors += '\nCommand not found'
+            exit_code = -1
+        except TimeoutExpired:
+            # remove todos os processos filhos e descendentes
+            pid = os.getpid()
+            # todos menos o primeiro da lista devem ser mortos
+            children = check_output(['pgrep', '-g', str(pid)]).decode('utf-8').splitlines()[1::]
+            children = children[::-1]
+            for child in children:
+                call(['kill', '-TERM', child])
+            output, errors = proc.communicate()  # segunda chance de terminar por bem
+            exit_code = proc.returncode or 1
+            errors += '\nERROR: Time limit exceeded %ss' % timeout
+
+        response[phase] = {
+            'stdout': output,
+            'stderr': errors,
+            'exit_code': exit_code,
+        }
+        if exit_code != 0:
+            break
+    return response
+
+
+if __name__ == '__main__':
+    linhas = ''.join(sys.stdin.readlines())
+    job = json.loads(linhas)
+    response = run(job.get('sourcetree'), job.get('commands'), job.get('input'))
+    json.dump(response, sys.stdout)
+    sys.exit(0)
---
+++ codebox/test/test_local.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python3
+
+import shutil
+from os.path import exists, join
+from src import codebox
+
+TIMEOUT = 0.1
+
+
+def save_sources(sourcetree, tempdir):
+    if exists(tempdir):
+        shutil.rmtree(tempdir)
+    codebox.save_sourcetree(sourcetree, tempdir)
+
+
+def test_save_sourcetree():
+    sourcetree = {
+        'a': 'aaaa',
+        'b': 'bbb',
+        'test/d': 'ddd',
+        'test/e': 'eee',
+        'images/f': 'fff',
+        'images/g': 'ggg',
+    }
+    tempdir = '/tmp/test_save_sourcetree'
+    save_sources(sourcetree, tempdir)
+    for filename in sourcetree.keys():
+        assert exists(join(tempdir, filename))
+
+
+def test_evaluate():
+    sourcetree = {
+        'source.py': '''import sys
+
+def outer(x):
+     def inner():
+            print(x)
+     return inner
+
+class someclass():
+
+    def wrongMethod():
+        aux = None
+        return
+''',
+    }
+    tempdir = '/tmp/test_evaluate'
+    save_sources(sourcetree, tempdir)
+    result = codebox.evaluate(sourcetree, tempdir)
+    assert 'source.py' in result['lint']
+    assert 'source.py' in result['metrics']
+    assert len(result['lint']['source.py']) > 0
+    assert isinstance(result['metrics']['source.py']['cyclomatic_complexity'], list)
+    assert 'source.py' not in result['metrics']['source.py']['loc']
+
+
+def test_exec_command_1():
+    commands = [('hello', 'echo 1 2 3', TIMEOUT)]
+    result = codebox.exec_commands(commands)
+    assert 'hello' in result
+    assert result['hello']['stdout'] == '1 2 3\n'
+    assert result['hello']['exit_code'] == 0
+
+
+def test_exec_python_hello():
+    sourcetree = {
+        'hello.py': 'import sys\n\nprint("Hello, world!")',
+    }
+    tempdir = '/tmp/test_exec_python_hello'
+    save_sources(sourcetree, tempdir)
+    command = [('exec', 'python3 hello.py', TIMEOUT)]
+    result = codebox.exec_commands(command, ref_dir=tempdir)
+    assert 'exec' in result
+    assert result['exec']['stdout'] == 'Hello, world!\n'
+    assert result['exec']['exit_code'] == 0
+
+
+def test_program_error():
+    '''
+    Python program with a syntax error
+    '''
+    source = '''import os
+
+sys.stdout.write('Olá mundo!')'''
+    sourcetree = {'hello.py': source}
+    tempdir = 'test_program_error'
+    save_sources(sourcetree, tempdir)
+    command = [
+        ('execution', 'python3 hello.py', TIMEOUT),
+    ]
+    resp = codebox.exec_commands(command, ref_dir=tempdir)
+    assert resp['execution']['stdout'] == ''
+    assert "NameError: name 'sys' is not defined" in resp['execution']['stderr']
+    assert resp['execution']['exit_code'] != 0
+
+
+def test_empty_source():
+    '''
+    Running a empty source
+    '''
+    sourcetree = {'empty.py': ''}
+    tempdir = '/tmp/test_empty_source'
+    save_sources(sourcetree, tempdir)
+    command = [('execution', 'python3 empty.py', TIMEOUT)]
+    resp = codebox.exec_commands(command, ref_dir=tempdir)
+    assert resp == {'execution': {'stderr': '', 'exit_code': 0, 'stdout': ''}}
+
+
+def test_empty_command():
+    '''
+    Running empty commands
+    '''
+    resp = codebox.exec_commands([])
+    assert resp == {}
+
+
+def test_input():
+    text = 'Hello\nWorld'
+    source = '''import sys
+
+for line in sys.stdin.readlines():
+    sys.stdout.write(line)
+'''
+    sourcetree = {
+        'input.py': source,
+    }
+    tempdir = '/tmp/test_input'
+    save_sources(sourcetree, tempdir)
+    command = [('test_input', 'python3 input.py', TIMEOUT)]
+    resp = codebox.exec_commands(command, input_=text, ref_dir=tempdir)
+    assert resp['test_input']['stdout'] == 'Hello\nWorld'
+    assert resp['test_input']['stderr'] == ''
+
+
+def test_utf_8_input():
+    text = 'Olá\nAçúcar'
+    source = '''import sys
+
+for line in sys.stdin.readlines():
+    sys.stdout.write(line)
+'''
+    sourcetree = {
+        'input.py': source,
+    }
+    tempdir = '/tmp/test_utf_8_input'
+    save_sources(sourcetree, tempdir)
+    command = [('utf_8', 'python3 input.py', TIMEOUT)]
+    resp = codebox.exec_commands(command, input_=text, ref_dir=tempdir)
+    assert resp['utf_8']['stdout'] == 'Olá\nAçúcar'
+    assert resp['utf_8']['stderr'] == ''
+
+
+def test_timeout():
+    source = 'import time\nprint("Going to sleep...")\ntime.sleep(5)\nprint("Overslept!")'
+    sourcetree = {
+        'timeout.py': source
+    }
+    tempdir = '/tmp/test_timeout'
+    save_sources(sourcetree, tempdir)
+    command = [('timeout', 'python3 timeout.py', TIMEOUT)]
+    resp = codebox.exec_commands(command, ref_dir=tempdir)
+    assert resp['timeout']['stdout'] == ''
+    assert 'ERROR: Time limit exceeded' in resp['timeout']['stderr']
+
+
+def test_multiple_commands():
+    source = '''#!/bin/bash
+exit 1
+'''
+    sourcetree = {'exit.sh': source}
+    tempdir = '/tmp/test_multiple_commands'
+    save_sources(sourcetree, tempdir)
+    commands = [
+        ('1', 'ls -l -h -a /srv', TIMEOUT),
+        ('2', 'echo abcd', TIMEOUT),
+        ('3', 'bash exit.sh', TIMEOUT),
+        ('4', 'echo nothing', TIMEOUT),
+    ]
+    result = codebox.exec_commands(commands, ref_dir=tempdir)
+    assert len(result) == 3
+
+
+def test_command_not_found():
+    commands = [
+        ('1', 'ls -l -h -a /srv', TIMEOUT),
+        ('2', 'echo abcd', TIMEOUT),
+        ('3', 'alksajkjalja', TIMEOUT),
+        ('4', 'echo nothing', TIMEOUT),
+    ]
+    result = codebox.exec_commands(commands)
+    assert len(result) == 3
+    assert 'Command not found' in result['3']['stderr']
+
+
+def test_save_path_sep():
+    '''
+    Verifica se nomes de arquivos que começam com '/' estão sendo devidamente
+    tratados
+    '''
+    sourcetree = {
+        '/tmp/lixo.sh': 'echo "lixo"',
+    }
+    tempdir = '/tmp/test_save_path_sep'
+    save_sources(sourcetree, tempdir)
+    assert exists('/tmp/test_save_path_sep/tmp/lixo.sh')
+
+
+def test_run():
+    text = 'Test\nRun'
+    source = '''import sys
+
+for line in sys.stdin.readlines():
+    sys.stdout.write(line)
+'''
+    sourcetree = {
+        '__init__.py': '',
+        'test.py': source,
+        'doc/index.rst': 'Title\n=====\n\nOne paragraph'
+    }
+    commands = [
+        ('bash', 'echo hello wold', 0.1),
+        ('python', 'python3 test.py', 0.1),
+    ]
+    result = codebox.run(sourcetree, commands, input_=text)
+    assert 'lint' in result
+    assert 'metrics' in result
+    assert result['python']['stdout'] == 'Test\nRun'
+
+
+def test_run_nothing():
+    result = codebox.run()
+    assert result == {}
+
+
+def test_run_no_sourcetree():
+    result = codebox.run(commands=[('bash', 'echo test', TIMEOUT)])
+    assert result['bash']['stdout'] == 'test\n'
+
+
+def test_no_commands():
+    sourcetree = {
+        '__init__.py': '',
+        'test.py': '#!/usr/bin/python3',
+        'doc/index.rst': 'Title\n=====\n\nOne paragraph'
+    }
+    result = codebox.run(sourcetree)
+    assert 'lint' in result and 'test.py' in result['lint']
+    assert '__init__.py' in result['metrics']
