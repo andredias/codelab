b5ce1e783294ce1653bd7754ade7dbff55fc18a8
André Felipe Dias <andre.dias@pronus.io>
2021-02-14 02:42:45 -0300

    Pydantic models use orjson as default

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,28 @@
 from typing import Optional
 
-from pydantic import BaseModel
+import orjson
+from pydantic import BaseModel as _BaseModel
+
+# ref: https://pydantic-docs.helpmanual.io/usage/exporting_models/#custom-json-deserialisation
+
+
+def orjson_dumps(v, *, default):
+    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+    return orjson.dumps(v, default=default).decode()
+
+
+class BaseModel(_BaseModel):
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
 
 Sourcefiles = dict[str, str]
 
 
 class Command(BaseModel):
-    type: Optional[str]
+    type: Optional[str] = None
     command: str
     timeout: Optional[float] = None
     input: Optional[str] = None
@@ -20,10 +36,7 @@ class CodeboxInput(BaseModel):
 class Response(BaseModel):
     stdout: str = ''
     stderr: str = ''
-    exit_code: int
-
-
-Responses = list[Response]
+    exit_code: int = 0
 
 
 class Description(BaseModel):
@@ -33,9 +46,9 @@ class Description(BaseModel):
 
 class Project(Description, CodeboxInput):
     id: str
-    responses: Responses = []
+    responses: list[Response] = []
 
 
 class ProjectResponses(BaseModel):
     id: str
-    responses: Responses = []
+    responses: list[Response] = []
