1538d8c89c99850b25ca1c9f0f3aff30ca0cbaee
André Felipe Dias <andref.dias@gmail.com>
2022-07-19 08:22:02 -0300

    configure docker-compose.yml to work in dev mode

--- backend/app/config.py
+++ backend/app/config.py
@@ -18,5 +18,5 @@ os.environ['LOGURU_DEBUG_COLOR'] = '<fg #777>'
 
 TTL = int(os.getenv('TTL', timedelta(days=3).total_seconds())) if not TESTING else 1
 TIMEOUT = float(os.getenv('TIMEOUT', 0.1))
-REDIS_URL = 'redis://localhost:6379' if DEBUG else 'redis://redis:6379'
-CODEBOX_URL = 'http://localhost:8000' if DEBUG else 'http://codebox:8000'
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+CODEBOX_URL = os.getenv('CODEBOX_URL', 'http://localhost:8000')
